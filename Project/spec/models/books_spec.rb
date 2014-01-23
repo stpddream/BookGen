048fb996b5aa185496b9861501487ed665b1c9f9
Panda <stmishu@gmail.com>
2014-01-23 17:24:01 -0500

    [Panda] remove unnecessary tests.

--- Project/spec/models/books_spec.rb
+++ Project/spec/models/books_spec.rb
@@ -1,17 +1,6 @@
-require 'spec_helper'
 
-describe Books do
-  before { 
-    @book = Book.new(name: "The Great Kate and her Seven Xuedis", 
-    isbn: "978-1934356142", author: "Jacy Li", publisher: "Panda Press", edition: "13th",
-    description: "Almost new.", status: "Unavailable")
-    }
-    
-    subject { @user }
-    
-    it { should respond_to(:name) }
-    it { should respond_to(:author) }
-    it { should respond_to(:isbn) }
-    
+describe "Books" do
+
+    pending { "hahaahaha "}    
     
 end
